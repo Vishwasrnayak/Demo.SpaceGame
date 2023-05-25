d4bf92efbc4aadddc5ef8b49bb74850bca413388
Vishwas R <123068708+Vishwasrnayak@users.noreply.github.com>
2023-05-25 09:58:06 +0530

    Delete Tailspin.SpaceGame.Database directory

--- Tailspin.SpaceGame.Database/dbo/Tables/Achievements.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Achievements] (
-    [id]            INT           NOT NULL,
-    [description]   NVARCHAR (50)    NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
--- Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
+++
@@ -1,7 +0,0 @@
-﻿
-CREATE TABLE [dbo].[Profiles] (
-    [id]                INT           NOT NULL,
-    [userName]          NVARCHAR (50) NOT NULL,
-    [avatarUrl]         NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
